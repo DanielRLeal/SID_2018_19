d34600088b7dec3805090d970fa987fe30580d72
havms-iscteiul <44034134+havms-iscteiul@users.noreply.github.com>
2019-04-17 20:27:00 +0100

    Primeira fase do MongoDB

---
+++ src/MongoBD/MongoRead.java
@@ -0,0 +1,85 @@
+package MongoBD;
+
+import com.mongodb.*;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoDatabase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.*;
+
+import org.bson.Document;
+
+public class MongoRead   {
+	public static void main(String[] args) {		
+		String queryTemperatura = "INSERT INTO medicoestemperatura (DataHoraMedicao, ValorMedicaoTemperatura) VALUES ";
+		String queryLuminosidade = "INSERT INTO medicoesluminosidade (DataHoraMedicao, ValorMedicaoLuminosidade) VALUES ";
+		
+		/*************Ler os dados da MongoBD*************/
+		
+		/*************Iniciar Ligação com o MongoBD*************/
+		//ligação á instancia mongo de porta 27017
+		MongoClient mongoClient1 = new MongoClient("127.0.0.1", 27017);
+        //vai buscar a bd de sid
+		DB db = mongoClient1.getDB("sid_mongobd");
+		//Colecção a inserir os dados
+		DBCollection table = db.getCollection("DadosSensor");
+		
+		DBCursor myCursor = table.find();
+		while (myCursor.hasNext()) {
+			DBObject jsonObject = myCursor.next(); //todo o objecto
+			if(!jsonObject.containsKey("ValorTemperatura")
+					|| !jsonObject.containsKey("ValorLuminosidade") 
+					|| !jsonObject.containsKey("DataHoraMedicao"))
+				continue;
+			queryTemperatura += "('"+jsonObject.get("DataHoraMedicao").toString()
+					+ "','" + jsonObject.get("ValorTemperatura").toString() + "')";
+			queryLuminosidade += "('"+jsonObject.get("DataHoraMedicao").toString()
+					+ "','" + jsonObject.get("ValorLuminosidade").toString() + "')";
+			
+			if(myCursor.hasNext())
+			{
+				queryTemperatura += ",";
+				queryLuminosidade += ",";
+			}else{
+				queryTemperatura += ";";
+				queryLuminosidade += ";";
+			}
+		}
+
+		System.out.println(queryLuminosidade);
+		System.out.println(queryTemperatura);
+		
+		/*************Inserir os dados na MySQL*************/
+		//caminho para a instancia MySQL
+		String servidor = "jdbc:mysql://localhost:3306/";
+		//base dados de sid
+		String baseDados = "sid_bd_php";
+		//driver da bd MySQL
+		String driver = "com.mysql.jdbc.Driver";
+		//nome do utilizador
+		String username = "root";
+		//password do utilizador
+		String pass = "";
+		try {
+			Class.forName(driver);
+			
+			//inicia uma conecção com a base de dados sid MySQL
+			Connection connection = DriverManager.getConnection(servidor + baseDados, username, pass);
+			System.out.println("connectou");
+			//Prepara para correr uma query SQL
+			Statement statement = connection.createStatement();
+			
+			//Executa a Query
+			statement.executeUpdate(queryLuminosidade);
+			statement.executeUpdate(queryTemperatura);
+			
+			connection.close();
+			table.remove(new BasicDBObject());
+			mongoClient1.close();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+    }
+}
---
+++ src/MongoBD/MongoWrite.java
@@ -0,0 +1,165 @@
+package MongoBD;
+
+import com.mongodb.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import org.bson.json.*;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttTopic;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+public class MongoWrite implements MqttCallback{
+	//Nome do topico subscrito
+	String topic 		= "sid_2019_g19_";
+	//conteudo a escrever no servidor Paho, utilizado quando se for publicar mensagens
+	//String content      = "teste123";
+	//QoS 2 pois queremos rapidade de serviço mas que a mensagem apenas seja entregue uma vez
+	//preferimos velocidade pois o que nos interessa é a variação dos valores ao longo do tempo
+	int qos             = 0;
+	//Caminho do servidor Paho a utilizar na conecção
+    String broker       = "tcp://iot.eclipse.org:1883";
+    //Id do cliente a utilizar na connecção
+    String clientId     = "js-utility-452";
+    DBCollection table;
+    
+	public static void main(String[] args) {
+	    new MongoWrite();
+	}
+	
+	public MongoWrite(){
+		//Objecto que torna a ligação persistente, caso venha a ser utilizado
+		MemoryPersistence persistent = new MemoryPersistence();
+		try {
+			/*************Iniciar Ligação com o Paho*************/
+			//Cria o objecto cliente com os parametros do link de acesso e com o id de cliente
+			//A MemoryPersistence permite que caso a ligação seja perdida este guarda as mensagens de QoS 1 e 2
+			//em fila associadas ao ClientID para quando existir reconecção se receba as mensagens de volta
+			MqttClient sampleClient = new MqttClient(broker, clientId, persistent);
+			
+			//cleansession a true indica que a ligação não foi persistente
+			//ou seja é uma ligação nova que vai limpar as mensagens da ligação antiga que ficaram em fila
+			//colocar a false para permitir receber mensagens que não foram entregues na sessão anterior
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            
+            //Connecta ao cliente
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            
+            //Define que é esta classe que vai implementar o CallBack
+            System.out.println("Connected");
+            sampleClient.setCallback(this);
+            
+            //Subscreve o topico no servidor
+            sampleClient.subscribe(topic);
+            
+            /*************Publica mensagem no Paho*************/
+            /*System.out.println("Publishing message: "+content);
+            //inicia objecto da mensagem a publicar
+            MqttMessage message = new MqttMessage(content.getBytes());
+            //Define a Qualidade de mensagem a utilizar
+            message.setQos(qos);
+            //publica a mensagem
+            sampleClient.publish(topic, message);
+            System.out.println("Message published");*/
+            
+            //desconecta a sessão
+            //sampleClient.disconnect();
+            //System.out.println("Disconnected");
+            //termina o programa
+            //System.exit(0);
+            
+            /*************Iniciar Ligação com o MongoBD*************/
+    		//ligação á instancia mongo de porta 27017
+            MongoClient mongoClient1 = new MongoClient("127.0.0.1", 27017);
+            //vai buscar a bd de sid
+    		DB db = mongoClient1.getDB("sid_mongobd");
+    		//Colecção a inserir os dados
+    		table = db.getCollection("DadosSensor");
+		} catch (MqttException e) {
+	        e.printStackTrace();
+	    }
+	}
+
+	/*************Ler mensagem do Paho*************/
+	@Override
+	public void messageArrived(String topicArrived, MqttMessage message) throws Exception {
+		//formato dos dados recebidos pelo sensor
+		//{"tmp":"22.40","hum":"61.30","dat":"9/4/2019","tim":"14:59:32","cell":"3138","sens":"wifi"}
+		
+		//no caso do topico não for igual ao da mensagem que estamos a espera sai da função
+		//ou no caso da qualidade de serviço ser outra não é de confiança
+		if(!this.topic.equals(topicArrived) || message.getQos() != qos)
+			return;
+		
+		if(!isJSONValid(message.toString()))
+		{
+			System.out.println("mensagem de formato JSON incorrecto:" + message.toString());
+			return;
+		}
+		
+		JSONObject jsonObj = new JSONObject(message.toString());
+		
+		if(!jsonObj.has("dat") || !jsonObj.has("tmp") || !jsonObj.has("cell"))
+		{
+			System.out.println("mensagem não contêm todos os dados necessários (dat, tmp, cell)");
+			return;
+		}
+		
+		//valor da data
+		String dataValor = jsonObj.get("dat").toString();
+		//valor da temperatura
+		String tempValor = jsonObj.get("tmp").toString();
+		//valor da luminosidade
+		String lumValor = jsonObj.get("cell").toString();
+		System.out.println("data:" + dataValor + "; Temp:" + tempValor + "; Luz:" + lumValor + ";");
+		
+		/*************Escrever dados do sensor na MongoBD*************/		
+		//documento a inserir na coleção
+		BasicDBObject document = new BasicDBObject();
+		document.put("ValorTemperatura", tempValor);
+		document.put("ValorLuminosidade", lumValor);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date(dataValor);
+        document.put("DataHoraMedicao", dateFormat.format(date));
+        System.out.println(document.toString());
+        //insere o documento na coleção
+		try { table.insert(document);} catch (Exception e) {}
+	}
+	
+	@Override
+	public void connectionLost(Throwable arg0) {
+	}
+
+	@Override
+	public void deliveryComplete(IMqttDeliveryToken arg0) {	
+	}
+	
+	public boolean isJSONValid(String test) {
+	    try {
+	        new JSONObject(test);
+	    } catch (JSONException ex) {
+	        // edited, to include @Arthur's comment
+	        // e.g. in case JSONArray is valid as well...
+	        try {
+	            new JSONArray(test);
+	        } catch (JSONException ex1) {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+}	
