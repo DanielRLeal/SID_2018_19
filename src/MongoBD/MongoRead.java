acb99e187693faadb1a8c67ebcdcb5668fb20fd5
havms-iscteiul <44034134+havms-iscteiul@users.noreply.github.com>
2019-04-20 00:49:40 +0100

    Coloquei a periodicidade na migração mongo->MySQL

--- src/MongoBD/MongoRead.java
+++ src/MongoBD/MongoRead.java
@@ -8,76 +8,82 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import org.bson.Document;
 
 public class MongoRead   {
-	public static void main(String[] args) {		
-		String queryTemperatura = "INSERT INTO medicoestemperatura (DataHoraMedicao, ValorMedicaoTemperatura) VALUES ";
-		String queryLuminosidade = "INSERT INTO medicoesluminosidade (DataHoraMedicao, ValorMedicaoLuminosidade) VALUES ";
-		
-		/*************Ler os dados da MongoBD*************/
-		
-		/*************Iniciar Ligação com o MongoBD*************/
-		//ligação á instancia mongo de porta 27017
-		MongoClient mongoClient1 = new MongoClient("127.0.0.1", 27017);
-        //vai buscar a bd de sid
-		DB db = mongoClient1.getDB("sid_mongobd");
-		//Colecção a inserir os dados
-		DBCollection table = db.getCollection("DadosSensor");
-		
-		DBCursor myCursor = table.find();
-		while (myCursor.hasNext()) {
-			DBObject jsonObject = myCursor.next(); //todo o objecto
-			if(!jsonObject.containsKey("ValorTemperatura")
-					|| !jsonObject.containsKey("ValorLuminosidade") 
-					|| !jsonObject.containsKey("DataHoraMedicao"))
-				continue;
-			queryTemperatura += "('"+jsonObject.get("DataHoraMedicao").toString()
-					+ "','" + jsonObject.get("ValorTemperatura").toString() + "')";
-			queryLuminosidade += "('"+jsonObject.get("DataHoraMedicao").toString()
-					+ "','" + jsonObject.get("ValorLuminosidade").toString() + "')";
-			
-			if(myCursor.hasNext())
-			{
-				queryTemperatura += ",";
-				queryLuminosidade += ",";
-			}else{
-				queryTemperatura += ";";
-				queryLuminosidade += ";";
-			}
-		}
-
-		System.out.println(queryLuminosidade);
-		System.out.println(queryTemperatura);
-		
-		/*************Inserir os dados na MySQL*************/
-		//caminho para a instancia MySQL
-		String servidor = "jdbc:mysql://localhost:3306/";
-		//base dados de sid
-		String baseDados = "sid_bd_php";
-		//driver da bd MySQL
-		String driver = "com.mysql.jdbc.Driver";
-		//nome do utilizador
-		String username = "root";
-		//password do utilizador
-		String pass = "";
+	public static void main(String[] args) {
 		try {
-			Class.forName(driver);
+			while(true){
+				String queryTemperatura = "INSERT INTO medicoestemperatura (DataHoraMedicao, ValorMedicaoTemperatura) VALUES ";
+				String queryLuminosidade = "INSERT INTO medicoesluminosidade (DataHoraMedicao, ValorMedicaoLuminosidade) VALUES ";
+				
+				/*************Ler os dados da MongoBD*************/
+				
+				/*************Iniciar Ligação com o MongoBD*************/
+				//ligação á instancia mongo de porta 27017
+				MongoClient mongoClient1 = new MongoClient("127.0.0.1", 27017);
+		        //vai buscar a bd de sid
+				DB db = mongoClient1.getDB("sid_mongobd");
+				//Colecção a inserir os dados
+				DBCollection table = db.getCollection("DadosSensor");
 			
-			//inicia uma conecção com a base de dados sid MySQL
-			Connection connection = DriverManager.getConnection(servidor + baseDados, username, pass);
-			System.out.println("connectou");
-			//Prepara para correr uma query SQL
-			Statement statement = connection.createStatement();
+				DBCursor myCursor = table.find();
+				while (myCursor.hasNext()) {
+					DBObject jsonObject = myCursor.next(); //todo o objecto
+					if(!jsonObject.containsKey("ValorTemperatura")
+							|| !jsonObject.containsKey("ValorLuminosidade") 
+							|| !jsonObject.containsKey("DataHoraMedicao"))
+						continue;
+					queryTemperatura += "('"+jsonObject.get("DataHoraMedicao").toString()
+							+ "','" + jsonObject.get("ValorTemperatura").toString() + "')";
+					queryLuminosidade += "('"+jsonObject.get("DataHoraMedicao").toString()
+							+ "','" + jsonObject.get("ValorLuminosidade").toString() + "')";
+					
+					if(myCursor.hasNext())
+					{
+						queryTemperatura += ",";
+						queryLuminosidade += ",";
+					}else{
+						queryTemperatura += ";";
+						queryLuminosidade += ";";
+					}
+				}
+		
+				System.out.println(queryLuminosidade);
+				System.out.println(queryTemperatura);
+				
+				/*************Inserir os dados na MySQL*************/
+				//caminho para a instancia MySQL
+				String servidor = "jdbc:mysql://localhost:3306/";
+				//base dados de sid
+				String baseDados = "sid_bd_php";
+				//driver da bd MySQL
+				String driver = "com.mysql.jdbc.Driver";
+				//nome do utilizador
+				String username = "root";
+				//password do utilizador
+				String pass = "";
 			
-			//Executa a Query
-			statement.executeUpdate(queryLuminosidade);
-			statement.executeUpdate(queryTemperatura);
+				Class.forName(driver);
+				
+				//inicia uma conecção com a base de dados sid MySQL
+				Connection connection = DriverManager.getConnection(servidor + baseDados, username, pass);
+				System.out.println("connectou");
+				//Prepara para correr uma query SQL
+				Statement statement = connection.createStatement();
+				
+				//Executa a Query
+				statement.executeUpdate(queryLuminosidade);
+				statement.executeUpdate(queryTemperatura);
+				
+				connection.close();
+				table.remove(new BasicDBObject());
+				mongoClient1.close();
 			
-			connection.close();
-			table.remove(new BasicDBObject());
-			mongoClient1.close();
+				TimeUnit.SECONDS.sleep(30);
+			}
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
